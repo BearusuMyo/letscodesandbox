170209417b9ba4c3a9996b8456bc5ffa12e0915b
Hiago Cardoso de Melo <hiago.go.mobile@gmail.com>
2022-01-31 20:45:41 -0400

    Redirect function deattached from pooh function

--- Aula_04/Exercício_01/js/onichan.js
+++ Aula_04/Exercício_01/js/onichan.js
@@ -1,4 +1,8 @@
-function pooh(){
-    alert("9999999999999 social credits added!\n9999999999999社會信用已添加！");
+function redirect(){
     return window.location.href = "https://bearusumyo.github.io/letscodesandbox/Aula_04/Exercício_01/dynaimg.html";
 }
+function pooh(){
+    alert("9999999999999 social credits added!\n9999999999999社會信用已添加！");
+    then redirect();
+}
+
