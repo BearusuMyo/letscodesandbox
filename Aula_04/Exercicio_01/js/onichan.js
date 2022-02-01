d3aab46efcd9c9d97e2d37823fff6d6c2a2e4b5d
Hiago Cardoso de Melo <hiago.go.mobile@gmail.com>
2022-01-31 20:48:59 -0400

    Fixed names

--- Aula_04/Exercicio_01/js/onichan.js
+++ Aula_04/Exercicio_01/js/onichan.js
@@ -1,5 +1,5 @@
 function redirect(){
-    return window.location.href = "https://bearusumyo.github.io/letscodesandbox/Aula_04/Exercício_01/dynaimg.html";
+    return window.location.href = "https://bearusumyo.github.io/letscodesandbox/Aula_04/Exercicio_01/dynaimg.html";
 }
 function pooh(){
     alert("9999999999999 social credits added!\n9999999999999社會信用已添加！");
