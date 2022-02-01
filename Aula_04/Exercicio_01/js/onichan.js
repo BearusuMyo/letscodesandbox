d1198f57da566a17b360626c60c24be91c5672d0
Hiago Cardoso de Melo <hiago.go.mobile@gmail.com>
2022-01-31 21:00:27 -0400

    Functions changed

--- Aula_04/Exercicio_01/js/onichan.js
+++ Aula_04/Exercicio_01/js/onichan.js
@@ -2,6 +2,6 @@ function redirect(){
     return window.location.href = "https://bearusumyo.github.io/letscodesandbox/Aula_04/Exercicio_01/dynaimg.html";
 }
 function pooh(){
-    alert("9999999999999 social credits added!\n9999999999999社會信用已添加！");
+    alert("9999999999999+ Serasa score!\nSeu score subiu 9999999999999 pontos!");
 }
 
