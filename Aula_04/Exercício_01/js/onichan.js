6c20db0a441dd627859c83a15c89f73653f5d8b6
Hiago Cardoso de Melo <hiago.go.mobile@gmail.com>
2022-01-31 20:37:58 -0400

    updated onichan.js

--- Aula_04/Exercício_01/js/onichan.js
+++ Aula_04/Exercício_01/js/onichan.js
@@ -1,4 +1,4 @@
 function pooh(){
     alert("9999999999999 social credits added!\n9999999999999社會信用已添加！");
-    window.location.pathname= "https://bearusumyo.github.io/letscodesandbox/Aula_04/Exerc%C3%ADcio_01/dynaimg.html";
+    window.location.href = "https://bearusumyo.github.io/letscodesandbox/Aula_04/Exerc%C3%ADcio_01/dynaimg.html";
 }
